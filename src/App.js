707e051c6a051a42ea1c2a2cf2a56b0e3b8add6c
officialaaryanx <avnisharyan2020@gmail.com>
2026-05-30 13:01:04 +0530

    fix: support GitHub Pages routes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Home from './pages/Home';
 import GameDetail from './pages/GameDetail';
 import './App.css';
 
+const routerBasename = process.env.PUBLIC_URL
+  ? new URL(process.env.PUBLIC_URL, window.location.origin).pathname.replace(/\/$/, '')
+  : '';
+
 function App() {
   return (
-    <Router>
+    <Router basename={routerBasename}>
       <div className="app">
         <Header />
         <main className="main-content">
